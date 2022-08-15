16073eb1b1e35981fdb5aa9d72a692242ef2ff23
Olaniyi Enigbokan <myjessy.sr@gmail.com>
2022-08-15 22:32:11 +0100

    Update portal.php

--- portal.php
+++ portal.php
@@ -55,7 +55,7 @@
 
         <!-- Main Content -->
         <div id="content">
-        <?php phpinfo(); ?>
+  
           <!-- Topbar -->
           <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
